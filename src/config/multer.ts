acad0abcaf63b94d5e431df1f5d1ac98c0741361
leonardofaria00 <leonardofaria00@gmail.com>
2020-09-21 23:46:47 -0300

    Add upload on AWS

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,10 +1,11 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
-export default {
-  dest: path.resolve(__dirname, '..', '..', 'uploads'),
-  storage: multer.diskStorage({
+const StorageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, callback) => {
       callback(null, path.resolve(__dirname, '..', '..', 'uploads'));
     },
@@ -14,6 +15,22 @@ export default {
       callback(null, filename);
     },
   }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: process.env.AWS_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: function (req, file, callback) {
+      const filename = `${uuidv4()} - ${file.originalname}`;
+
+      callback(null, filename);
+    },
+  }),
+};
+
+export default {
+  dest: path.resolve(__dirname, '..', '..', 'uploads'),
+  storage: StorageTypes[process.env.LOCAL_STORAGE],
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
